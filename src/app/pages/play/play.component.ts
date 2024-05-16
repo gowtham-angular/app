1b9c7ba3397f6c147b81c9fda12fe215a37c23ec
gowthamkumar <gowthamkumardoit@gmail.com>
2024-05-16 16:22:38 +0530

    Added counter

--- src/app/pages/play/play.component.ts
+++ src/app/pages/play/play.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ProductsService } from '../../service/products.service';
 import { UserService } from '../../service/user.service';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
@@ -7,6 +7,7 @@ import { DetailsDialogComponent } from '../../common/details-dialog/details-dial
 import { MatDialog } from '@angular/material/dialog';
 import { FirestoreService } from '../../service/firestore.service';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { take } from 'rxjs';
 
 
 @Component({
@@ -14,7 +15,7 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
   templateUrl: './play.component.html',
   styleUrl: './play.component.scss'
 })
-export class PlayComponent {
+export class PlayComponent implements OnDestroy {
 
   products: any;
   user: any;
@@ -23,13 +24,13 @@ export class PlayComponent {
   totalAmount!: number;
   totalCountFlag!: boolean;
   vipTwoFlag!: boolean;
+  isMissionComplete!: boolean;
 
   constructor(private _productService: ProductsService,
     private userService: UserService, private auth: AngularFireAuth, private utilService: UtilsService,
-    public dialog: MatDialog, private fireStoreService: FirestoreService, private fireStore : AngularFirestore
+    public dialog: MatDialog, private fireStoreService: FirestoreService, private fireStore: AngularFirestore
   ) {
     this.getProducts();
-
     this.userService.getUserData().subscribe((users: any) => {
       this.getAuthenticatedUser(users);
     });
@@ -41,28 +42,56 @@ export class PlayComponent {
       if (this.user?.totalAmount < 1) {
         this.utilService.getSnackBar("Please Recharge before start the tasks.")
       } else {
+
         const dialogRef = this.dialog.open(DetailsDialogComponent);
-
         dialogRef.afterClosed().subscribe((result: any) => {
-
           this.utilService.isOrderSubmitted.next(true);
         });
       }
+
     }
   }
 
   startVipTwo() {
+
     if (this.user) {
+      if (this.user.totalInvested >= 0) {
+        this.isMissionComplete = true;
 
-      if (this.user?.totalAmount < 1500) {
-        this.utilService.getSnackBar("Please Recharge Before Start the Day Two Tasks.");
+        if (this.user?.totalAmount < 1500) {
+          this.utilService.getSnackBar("Please Recharge Before Start the Day Two Tasks.");
+        } else {
+
+          this.utilService.getCount(this.user?.id).pipe(take(1)).subscribe((data: any) => {
+            if (data) {
+              let count = data.count;
+
+              if (count === 38) {
+                this.fireStore.collection('users').doc(this.user.id).update({
+                  totalInvested: -1555
+                })
+              }
+
+              if (count === 40) {
+                this.fireStore.collection('users').doc(this.user.id).update({
+                  totalInvested: -555
+                })
+              }
+            }
+
+          })
+
+          const dialogRef = this.dialog.open(DetailsDialogComponent);
+
+          dialogRef.afterClosed().subscribe((result: any) => {
+            this.utilService.isVipTwoEnabled.next(true);
+          });
+        }
       } else {
-        const dialogRef = this.dialog.open(DetailsDialogComponent);
-
-        dialogRef.afterClosed().subscribe((result: any) => {
-          this.utilService.isVipTwoEnabled.next(true);
-        });
+        this.isMissionComplete = false;
+        this.utilService.getSnackBar("Please complete the mission");
       }
+
     }
   }
 
@@ -110,7 +139,7 @@ export class PlayComponent {
         this.submittedData = data.arrayField;
         this.totalCount = this.submittedData?.length;
 
-        if(this.totalCount > 19) {
+        if (this.totalCount > 19) {
           this.totalCountFlag = true;
           this.vipTwoFlag = true;
 
@@ -123,7 +152,7 @@ export class PlayComponent {
         this.submittedData.forEach((element: any) => {
           count += element.price;
         });
-        this.totalAmount =  (count * 1.5);
+        this.totalAmount = (count * 1.5);
         this.utilService.taskAmount.next(this.totalAmount);
         this.utilService.updateCount(this.user?.id, this.totalCount);
 
@@ -131,6 +160,9 @@ export class PlayComponent {
     });
   }
 
+  ngOnDestroy() {
+
+  }
 }
 
 
